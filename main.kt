7d4773870f54a0b6c4cac110789dfff17bd080b1
Hari-42 <harisaranmohanathas@hotmail.com>
2025-03-23 13:04:36 +0100

    added text

--- main.kt
+++ main.kt
@@ -1,16 +1,32 @@
 import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.JLabel
 import java.awt.Color
 import java.awt.Dimension
+import java.awt.Font
+
+
 
 fun main() {
     val frame = JFrame("Stopwatch")
 
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
+
+
+    val label = JLabel("Stopwatch")
+    label.foreground = Color.WHITE
+    label.font = Font("Roboto", Font.BOLD, 48)
+
+
+
     val panel = JPanel()
     panel.background = Color.BLACK
     panel.preferredSize = Dimension(800, 600)
+
+
+    panel.add(label)
+
 
     frame.contentPane.add(panel)
 
